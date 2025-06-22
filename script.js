7cb00919dae82f6c313b3982ea6c0044b4a782e6
shivam78775 <148251171+shivam78775@users.noreply.github.com>
2025-06-23 00:13:21 +0530

    Added the reload animation and main text

---
+++ script.js
@@ -0,0 +1,25 @@
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+var tl = gsap.timeline()
+
+tl.to("#page1",{
+    y:"50vh",
+    scale:0.4,
+    duration:0
+})
+tl.to("#page1",{
+    y:"-50vh",
+    duration:1,
+    delay:1
+})
+tl.to("#page1",{
+    y:"0vh",
+    rotate:360,
+    scale:1,
+    duration:0.7,
+    delay:0.5
+})
